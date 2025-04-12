ff7e54e64d84a4e9fa26a82a640b819bb6193357
DEV-VED-om <165093325+ViniciusXSantos@users.noreply.github.com>
2025-04-12 10:57:14 -0300

    tentativa de correção de graficos
    deem rollback se possivel, pode acabar causando danos

--- jte-classes/gg/jte/generated/ondemand/layout/JtemainGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/layout/JtemainGenerated.java
@@ -6,10 +6,10 @@ public final class JtemainGenerated {
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, gg.jte.Content content) {
 		jteOutput.writeContent("\r\n<!DOCTYPE html>\r\n<html lang=\"pt-BR\">\r\n\r\n<head>\r\n    <meta charset=\"UTF-8\">\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n    <title>Sustância - Home Page</title>\r\n    <link href=\"https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap\" rel=\"stylesheet\">\r\n    <link rel=\"stylesheet\" href=\"/css/style.css\">\r\n    <link rel=\"stylesheet\" href=\"/css/relatorio.css\">\r\n    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>\r\n    <link rel=\"stylesheet\"\r\n        href=\"https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=menu\" />\r\n</head>\r\n\r\n<body>\r\n\r\n    <div class=\"wrapper\">\r\n    ");
 		gg.jte.generated.ondemand.layout.JteasideGenerated.render(jteOutput, jteHtmlInterceptor);
-		jteOutput.writeContent("\r\n        <main class=\"main container\">\r\n            ");
-		jteOutput.setContext("main", null);
+		jteOutput.writeContent("\r\n        <div> class=\"main container\">\r\n            ");
+		jteOutput.setContext("div", null);
 		jteOutput.writeUserContent(content);
-		jteOutput.writeContent("\r\n        </main>\r\n        \r\n    </div>\r\n</body>\r\n\r\n</html>");
+		jteOutput.writeContent("\r\n        </div>\r\n        \r\n    </div>\r\n</body>\r\n\r\n</html>");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		gg.jte.Content content = (gg.jte.Content)params.get("content");
