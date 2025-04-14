f5f1646b093d7fe018bb378a8ce4d8df7b957c0d
VII The Programmer <165093325+ViniciusXSantos@users.noreply.github.com>
2025-04-13 22:33:39 -0300

    entrevistadores

--- jte-classes/gg/jte/generated/ondemand/layout/JteheaderGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/layout/JteheaderGenerated.java
@@ -4,7 +4,7 @@ public final class JteheaderGenerated {
 	public static final String JTE_NAME = "layout/header.jte";
 	public static final int[] JTE_LINE_INFO = {5,5,5,5,5,5,5,5,5,5,5,5};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
-		jteOutput.writeContent("<header>\n    <button class=\"menu-toggle\" id=\"menu-toggle\" onclick=\"toggleMenu()\">\n        <i class='bx bx-menu' alt=\"menu\"></i>\n    </button>\n</header>\n");
+		jteOutput.writeContent("<header>\r\n    <button class=\"menu-toggle\" id=\"menu-toggle\" onclick=\"toggleMenu()\">\r\n        <i class='bx bx-menu' alt=\"menu\"></i>\r\n    </button>\r\n</header>\r\n");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		render(jteOutput, jteHtmlInterceptor);
