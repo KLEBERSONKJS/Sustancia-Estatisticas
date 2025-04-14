be76cc89fb48feda98539a015efcbaf4d219093a
Kleberson <klebitcell@gmail.com>
2025-04-14 13:48:59 -0300

    date implementada

---
+++ jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
@@ -0,0 +1,18 @@
+package gg.jte.generated.ondemand;
+@SuppressWarnings("unchecked")
+public final class JteindexGenerated {
+	public static final String JTE_NAME = "index.jte";
+	public static final int[] JTE_LINE_INFO = {1,1,1,1,1,1,1,1,1,4,4,4,6,6,6,6,6,6};
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
+		jteOutput.writeContent("\n    ");
+		gg.jte.generated.ondemand.layout.JtemainGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\n        <img src=\"/assets/icons/cactinho.svg\" class=\"cactus\">\n\n    ");
+			}
+		});
+		jteOutput.writeContent("\n\n   ");
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		render(jteOutput, jteHtmlInterceptor);
+	}
+}
