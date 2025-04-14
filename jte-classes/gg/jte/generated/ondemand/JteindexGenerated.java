778944a00091139d26656d4830734a30bda13772
VII The Programmer <165093325+ViniciusXSantos@users.noreply.github.com>
2025-04-13 21:59:00 -0300

    formulario de cadastro horizontal

---
+++ jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
@@ -0,0 +1,18 @@
+package gg.jte.generated.ondemand;
+@SuppressWarnings("unchecked")
+public final class JteindexGenerated {
+	public static final String JTE_NAME = "index.jte";
+	public static final int[] JTE_LINE_INFO = {1,1,1,1,1,1,1,1,1,4,4,4,6,6,6,6,6,6};
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
+		jteOutput.writeContent("\r\n    ");
+		gg.jte.generated.ondemand.layout.JtemainGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\r\n    <p> SEJA BEM VINDO!</p>\r\n        <p>Coloque aqui a nossa logo ou alguma imagem que remete a nós</p>\r\n    ");
+			}
+		});
+		jteOutput.writeContent("\r\n\r\n   ");
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		render(jteOutput, jteHtmlInterceptor);
+	}
+}
